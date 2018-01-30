b0d5fe0a664a45962af3a144a8bd9ab95622027a
LittleHeap <418761605@qq.com>
2018-01-30 15:41:05 +0800

    update

--- 1.Python基础+数学库/(1) intro.py
+++ 1.Python基础+数学库/(1) intro.py
@@ -8,19 +8,20 @@ def residual(t, x, y):
 
 
 def residual2(t, x, y):
-    print
-    t[0], t[1]
+    print(t[0], t[1])
     return y - t[0] * np.sin(t[1] * x)
 
 
 # x ** x        x > 0
 # (-x) ** (-x)  x < 0
 def f(x):
-    y = np.ones_like(x)
+    y = np.empty_like(x)
     i = x > 0
     y[i] = np.power(x[i], x[i])
     i = x < 0
     y[i] = np.power(-x[i], -x[i])
+    # i = (x == 0)
+    # y[i] = 1
     return y
 
 
@@ -240,232 +241,267 @@ if __name__ == "__main__":
 
     '''
 
+    '''
+    import time
+    import math
     print("\n---------------------------\n")
     print("\n---------------------------\n")
     # 4. numpy与Python数学库的时间比较
-    # for j in np.logspace(0, 7, 10):
-    #     j = int(j)
-    #     x = np.linspace(0, 10, j)
-    #     start = time.clock()
-    #     y = np.sin(x)
-    #     t1 = time.clock() - start
-    #
-    #     x = x.tolist()
-    #     start = time.clock()
-    #     for i, t in enumerate(x):
-    #         x[i] = math.sin(t)
-    #     t2 = time.clock() - start
-    #     print j, ": ", t1, t2, t2/t1
+    for j in np.logspace(0, 7, 10):
+        j = int(j)
+        x = np.linspace(0, 10, j)
+        start = time.clock()
+        y = np.sin(x)
+        t1 = time.clock() - start
 
+        x = x.tolist()
+        start = time.clock()
+        for i, t in enumerate(x):
+            x[i] = math.sin(t)
+        t2 = time.clock() - start
+        print(j, ": ", t1, t2, t2/t1)
+    
+    '''
+    '''
+    import matplotlib.pyplot as plt
+    import matplotlib as mpl
+    import math
+
+    print("\n---------------------------\n")
+    print("\n---------------------------\n")
     # 5.绘图
     # 5.1 绘制正态分布概率密度函数
-    # mu = 0
-    # sigma = 1
-    # x = np.linspace(mu - 3 * sigma, mu + 3 * sigma, 50)
-    # y = np.exp(-(x - mu) ** 2 / (2 * sigma ** 2)) / (math.sqrt(2 * math.pi) * sigma)
-    # print x.shape
-    # print 'x = \n', x
-    # print y.shape
-    # print 'y = \n', y
-    # # plt.plot(x, y, 'ro-', linewidth=2)
-    # plt.plot(x, y, 'r-', x, y, 'go', linewidth=2, markersize=8)
-    # plt.grid(True)
-    # plt.show()
+    mpl.rcParams['font.sans-serif'] = [u'SimHei']  # FangSong/黑体 FangSong/KaiTi
+    mpl.rcParams['axes.unicode_minus'] = False
+    mu = 0
+    sigma = 1
+    x = np.linspace(mu - 3 * sigma, mu + 3 * sigma, 50)
+    y = np.exp(-(x - mu) ** 2 / (2 * sigma ** 2)) / (math.sqrt(2 * math.pi) * sigma)
+    print(x.shape)
+    print('x = \n', x)
+    print(y.shape)
+    print('y = \n', y)
+    # plt.plot(x, y, 'ro-', linewidth=2)
+    plt.plot(x, y, 'r-', x, y, 'go', linewidth=2, markersize=8)
+    plt.grid(True)
+    plt.show()
 
-    # matplotlib.rcParams['font.sans-serif'] = [u'SimHei']  #FangSong/黑体 FangSong/KaiTi
-    # matplotlib.rcParams['axes.unicode_minus'] = False
-
+    print("\n---------------------------\n")
     # 5.2 损失函数：Logistic损失(-1,1)/SVM Hinge损失/ 0/1损失
-    # x = np.array(np.linspace(start=-2, stop=3, num=1001, dtype=np.float))
-    # y_logit = np.log(1 + np.exp(-x)) / math.log(2)
-    # y_boost = np.exp(-x)
-    # y_01 = x < 0
-    # y_hinge = 1.0 - x
-    # y_hinge[y_hinge < 0] = 0
-    # plt.plot(x, y_logit, 'r-', label='Logistic Loss', linewidth=2)
-    # plt.plot(x, y_01, 'g-', label='0/1 Loss', linewidth=2)
-    # plt.plot(x, y_hinge, 'b-', label='Hinge Loss', linewidth=2)
-    # plt.plot(x, y_boost, 'm--', label='Adaboost Loss', linewidth=2)
-    # plt.grid()
-    # plt.legend(loc='upper right')
-    # # plt.savefig('1.png')
-    # plt.show()
+    x = np.array(np.linspace(start=-2, stop=3, num=1001, dtype=np.float))
+    y_logit = np.log(1 + np.exp(-x)) / math.log(2)
+    y_boost = np.exp(-x)
+    y_01 = x < 0
+    y_hinge = 1.0 - x
+    y_hinge[y_hinge < 0] = 0
+    plt.plot(x, y_logit, 'r-', label='Logistic Loss', linewidth=2)
+    plt.plot(x, y_01, 'g-', label='0/1 Loss', linewidth=2)
+    plt.plot(x, y_hinge, 'b-', label='Hinge Loss', linewidth=2)
+    plt.plot(x, y_boost, 'm--', label='Adaboost Loss', linewidth=2)
+    plt.grid()
+    plt.legend(loc='upper right')
+    # plt.savefig('1.png')
+    plt.show()
 
-    # # # 5.3 x^x
-    # x = np.linspace(-1.3, 1.3, 101)
-    # y = f(x)
-    # plt.plot(x, y, 'g-', label='x^x', linewidth=2)
-    # plt.grid()
-    # plt.legend(loc='upper left')
-    # plt.show()
+    # 5.3 x^x
+    x = np.linspace(-1.3, 1.3, 101)
+    y = f(x)
+    plt.plot(x, y, 'g-', label='x^x', linewidth=2)
+    plt.grid()
+    plt.legend(loc='upper left')
+    plt.show()
 
-    # # 5.4 胸型线
-    # x = np.arange(1, 0, -0.001)
-    # y = (-3 * x * np.log(x) + np.exp(-(40 * (x - 1 / np.e)) ** 4) / 25) / 2
-    # plt.figure(figsize=(5,7))
-    # plt.plot(y, x, 'r-', linewidth=2)
-    # plt.grid(True)
-    # plt.show()
+    # 5.4 胸型线
+    x = np.arange(1, 0, -0.001)
+    y = (-3 * x * np.log(x) + np.exp(-(40 * (x - 1 / np.e)) ** 4) / 25) / 2
+    plt.figure(figsize=(5, 7))
+    plt.plot(y, x, 'r-', linewidth=2)
+    plt.grid(True)
+    plt.show()
 
     # 5.5 心形线
-    # t = np.linspace(0, 7, 100)
-    # x = 16 * np.sin(t) ** 3
-    # y = 13 * np.cos(t) - 5 * np.cos(2*t) - 2 * np.cos(3*t) - np.cos(4*t)
-    # plt.plot(x, y, 'r-', linewidth=2)
-    # plt.grid(True)
-    # plt.show()
+    t = np.linspace(0, 7, 100)
+    x = 16 * np.sin(t) ** 3
+    y = 13 * np.cos(t) - 5 * np.cos(2 * t) - 2 * np.cos(3 * t) - np.cos(4 * t)
+    plt.plot(x, y, 'r-', linewidth=2)
+    plt.grid(True)
+    plt.show()
 
-    # # 5.6 渐开线
-    # t = np.linspace(0, 50, num=1000)
-    # x = t*np.sin(t) + np.cos(t)
-    # y = np.sin(t) - t*np.cos(t)
-    # plt.plot(x, y, 'r-', linewidth=2)
-    # plt.grid()
-    # plt.show()
+    # 5.6 渐开线
+    t = np.linspace(0, 50, num=1000)
+    x = t * np.sin(t) + np.cos(t)
+    y = np.sin(t) - t * np.cos(t)
+    plt.plot(x, y, 'r-', linewidth=2)
+    plt.grid()
+    plt.show()
 
-    # # Bar
-    # matplotlib.rcParams['font.sans-serif'] = [u'SimHei']  #黑体 FangSong/KaiTi
-    # matplotlib.rcParams['axes.unicode_minus'] = False
-    # x = np.arange(0, 10, 0.1)
-    # y = np.sin(x)
-    # plt.bar(x, y, width=0.04, linewidth=0.2)
-    # plt.plot(x, y, 'r--', linewidth=2)
-    # plt.title(u'Sin曲线')
-    # plt.xticks(rotation=-60)
-    # plt.xlabel('X')
-    # plt.ylabel('Y')
-    # plt.grid()
-    # plt.show()
+    # Bar
+    mpl.rcParams['font.sans-serif'] = [u'SimHei']  # 黑体 FangSong/KaiTi
+    mpl.rcParams['axes.unicode_minus'] = False
+    x = np.arange(0, 10, 0.1)
+    y = np.sin(x)
+    plt.bar(x, y, width=0.04, linewidth=0.2)
+    plt.plot(x, y, 'r--', linewidth=2)
+    plt.title(u'Sin曲线')
+    plt.xticks(rotation=-60)
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.grid()
+    plt.show()
 
-    # # 6. 概率分布
-    # # 6.1 均匀分布
-    # x = np.random.rand(10000)
-    # t = np.arange(len(x))
-    # plt.hist(x, 30, color='m', alpha=0.5)
-    # # plt.plot(t, x, 'r-', label=u'均匀分布')
-    # plt.legend(loc='upper left')
-    # plt.grid()
-    # plt.show()
-    #
-    # # # 6.2 验证中心极限定理
-    # t = 10000
-    # a = np.zeros(1000)
-    # for i in range(t):
-    #     a += np.random.uniform(-5, 5, 1000)
-    # a /= t
-    # plt.hist(a, bins=30, color='g', alpha=0.5, normed=True)
-    # plt.grid()
-    # plt.show()
+    '''
 
-    # # 6.3 Poisson分布
-    # x = np.random.poisson(lam=5, size=10000)
-    # print x
-    # pillar = 15
-    # a = plt.hist(x, bins=pillar, normed=True, range=[0, pillar], color='g', alpha=0.5)
-    # plt.grid()
-    # # plt.show()
-    # print a
-    # print a[0].sum()
+    '''
+    print("\n---------------------------\n")
+    print("\n---------------------------\n")
+    # 6. 概率分布
+    # 6.1 均匀分布
+    import matplotlib.pyplot as plt
 
-    # # 6.4 直方图的使用
-    # mu = 2
-    # sigma = 3
-    # data = mu + sigma * np.random.randn(1000)
-    # h = plt.hist(data, 30, normed=1, color='#a0a0ff')
-    # x = h[1]
-    # y = norm.pdf(x, loc=mu, scale=sigma)
-    # plt.plot(x, y, 'r--', x, y, 'ro', linewidth=2, markersize=4)
-    # plt.grid()
-    # plt.show()
+    x = np.random.rand(10000)
+    t = np.arange(len(x))
+    plt.hist(x, 30, color='m', alpha=0.5)
+    # plt.plot(t, x, 'r-', label=u'均匀分布')
+    plt.legend(loc='upper left')
+    plt.grid()
+    plt.show()
 
-    # # 6.5 插值
+    # # 6.2 验证中心极限定理
+    t = 10000
+    a = np.zeros(1000)
+    for i in range(t):
+        a += np.random.uniform(-5, 5, 1000)
+    a /= t
+    plt.hist(a, bins=30, color='g', alpha=0.5, normed=True)
+    plt.grid()
+    plt.show()
+
+    # 6.3 Poisson分布
+    x = np.random.poisson(lam=5, size=10000)
+    print(x)
+    pillar = 15
+    a = plt.hist(x, bins=pillar, normed=True, range=[0, pillar], color='g', alpha=0.5)
+    plt.grid()
+    plt.show()
+    print(a)
+    print(a[0].sum())
+
+    # 6.4 直方图的使用
+    from scipy.stats import norm
+    import scipy
+
+    mu = 2
+    sigma = 3
+    data = mu + sigma * np.random.randn(1000)
+    h = plt.hist(data, 30, normed=1, color='#a0a0ff')
+    x = h[1]
+    y = norm.pdf(x, loc=mu, scale=sigma)
+    plt.plot(x, y, 'r--', x, y, 'ro', linewidth=2, markersize=4)
+    plt.grid()
+    plt.show()
+
+    # 6.5 插值
     # rv = poisson(5)
     # x1 = a[1]
     # y1 = rv.pmf(x1)
     # itp = BarycentricInterpolator(x1, y1)  # 重心插值
     # x2 = np.linspace(x.min(), x.max(), 50)
     # y2 = itp(x2)
-    # cs = scipy.interpolate.CubicSpline(x1, y1)       # 三次样条插值
-    # plt.plot(x2, cs(x2), 'm--', linewidth=5, label='CubicSpine')           # 三次样条插值
-    # plt.plot(x2, y2, 'g-', linewidth=3, label='BarycentricInterpolator')   # 重心插值
-    # plt.plot(x1, y1, 'r-', linewidth=1, label='Actural Value')             # 原始值
+    # cs = scipy.interpolate.CubicSpline(x1, y1)  # 三次样条插值
+    # plt.plot(x2, cs(x2), 'm--', linewidth=5, label='CubicSpine')  # 三次样条插值
+    # plt.plot(x2, y2, 'g-', linewidth=3, label='BarycentricInterpolator')  # 重心插值
+    # plt.plot(x1, y1, 'r-', linewidth=1, label='Actural Value')  # 原始值
     # plt.legend(loc='upper right')
     # plt.grid()
     # plt.show()
 
-    # 7. 绘制三维图像
-    # x, y = np.ogrid[-3:3:100j, -3:3:100j]
-    # # u = np.linspace(-3, 3, 101)
-    # # x, y = np.meshgrid(u, u)
-    # z = x*y*np.exp(-(x**2 + y**2)/2) / math.sqrt(2*math.pi)
-    # # z = x*y*np.exp(-(x**2 + y**2)/2) / math.sqrt(2*math.pi)
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    # # ax.plot_surface(x, y, z, rstride=5, cstride=5, cmap=cm.coolwarm, linewidth=0.1)  #
-    # ax.plot_surface(x, y, z, rstride=5, cstride=5, cmap=cm.Accent, linewidth=0.5)
-    # plt.show()
-    # # cmaps = [('Perceptually Uniform Sequential',
-    # #           ['viridis', 'inferno', 'plasma', 'magma']),
-    # #          ('Sequential', ['Blues', 'BuGn', 'BuPu',
-    # #                          'GnBu', 'Greens', 'Greys', 'Oranges', 'OrRd',
-    # #                          'PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu',
-    # #                          'Reds', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd']),
-    # #          ('Sequential (2)', ['afmhot', 'autumn', 'bone', 'cool',
-    # #                              'copper', 'gist_heat', 'gray', 'hot',
-    # #                              'pink', 'spring', 'summer', 'winter']),
-    # #          ('Diverging', ['BrBG', 'bwr', 'coolwarm', 'PiYG', 'PRGn', 'PuOr',
-    # #                         'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'Spectral',
-    # #                         'seismic']),
-    # #          ('Qualitative', ['Accent', 'Dark2', 'Paired', 'Pastel1',
-    # #                           'Pastel2', 'Set1', 'Set2', 'Set3']),
-    # #          ('Miscellaneous', ['gist_earth', 'terrain', 'ocean', 'gist_stern',
-    # #                             'brg', 'CMRmap', 'cubehelix',
-    # #                             'gnuplot', 'gnuplot2', 'gist_ncar',
-    # #                             'nipy_spectral', 'jet', 'rainbow',
-    # #                             'gist_rainbow', 'hsv', 'flag', 'prism'])]
+    '''
 
+    '''
+    import math
+    import matplotlib.pyplot as plt
+    print("\n---------------------------\n")
+    print("\n---------------------------\n")
+    # 7. 绘制三维图像
+    x, y = np.ogrid[-3:3:100j, -3:3:100j]
+    # u = np.linspace(-3, 3, 101)
+    # x, y = np.meshgrid(u, u)
+    z = x*y*np.exp(-(x**2 + y**2)/2) / math.sqrt(2*math.pi)
+    # z = x*y*np.exp(-(x**2 + y**2)/2) / math.sqrt(2*math.pi)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    # ax.plot_surface(x, y, z, rstride=5, cstride=5, cmap=cm.coolwarm, linewidth=0.1)  #
+    ax.plot_surface(x, y, z, rstride=5, cstride=5, cmap=cm.Accent, linewidth=0.5)
+    plt.show()
+    # cmaps = [('Perceptually Uniform Sequential',
+    #           ['viridis', 'inferno', 'plasma', 'magma']),
+    #          ('Sequential', ['Blues', 'BuGn', 'BuPu',
+    #                          'GnBu', 'Greens', 'Greys', 'Oranges', 'OrRd',
+    #                          'PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu',
+    #                          'Reds', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd']),
+    #          ('Sequential (2)', ['afmhot', 'autumn', 'bone', 'cool',
+    #                              'copper', 'gist_heat', 'gray', 'hot',
+    #                              'pink', 'spring', 'summer', 'winter']),
+    #          ('Diverging', ['BrBG', 'bwr', 'coolwarm', 'PiYG', 'PRGn', 'PuOr',
+    #                         'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'Spectral',
+    #                         'seismic']),
+    #          ('Qualitative', ['Accent', 'Dark2', 'Paired', 'Pastel1',
+    #                           'Pastel2', 'Set1', 'Set2', 'Set3']),
+    #          ('Miscellaneous', ['gist_earth', 'terrain', 'ocean', 'gist_stern',
+    #                             'brg', 'CMRmap', 'cubehelix',
+    #                             'gnuplot', 'gnuplot2', 'gist_ncar',
+    #                             'nipy_spectral', 'jet', 'rainbow',
+    #                             'gist_rainbow', 'hsv', 'flag', 'prism'])]
+
+    '''
+
+    '''
+    print("\n---------------------------\n")
+    print("\n---------------------------\n")
     # 8.1 scipy
     # 线性回归例1
-    # x = np.linspace(-2, 2, 50)
-    # A, B, C = 2, 3, -1
-    # y = (A * x ** 2 + B * x + C) + np.random.rand(len(x))*0.75
-    #
-    # t = leastsq(residual, [0, 0, 0], args=(x, y))
-    # theta = t[0]
-    # print '真实值：', A, B, C
-    # print '预测值：', theta
-    # y_hat = theta[0] * x ** 2 + theta[1] * x + theta[2]
-    # plt.plot(x, y, 'r-', linewidth=2, label=u'Actual')
-    # plt.plot(x, y_hat, 'g-', linewidth=2, label=u'Predict')
-    # plt.legend(loc='upper left')
-    # plt.grid()
-    # plt.show()
+    import matplotlib.pyplot as plt
 
-    # # 线性回归例2
-    # x = np.linspace(0, 5, 100)
-    # A = 5
-    # w = 1.5
-    # y = A * np.sin(w*x) + np.random.rand(len(x)) - 0.5
-    #
-    # t = leastsq(residual2, [3, 1], args=(x, y))
-    # theta = t[0]
-    # print '真实值：', A, w
-    # print '预测值：', theta
-    # y_hat = theta[0] * np.sin(theta[1] * x)
-    # plt.plot(x, y, 'r-', linewidth=2, label='Actual')
-    # plt.plot(x, y_hat, 'g-', linewidth=2, label='Predict')
-    # plt.legend(loc='lower left')
-    # plt.grid()
-    # plt.show()
+    x = np.linspace(-2, 2, 50)
+    A, B, C = 2, 3, -1
+    y = (A * x ** 2 + B * x + C) + np.random.rand(len(x)) * 0.75
+    t = leastsq(residual, [0, 0, 0], args=(x, y))
+    theta = t[0]
+    print('真实值：', A, B, C)
+    print('预测值：', theta)
+    y_hat = theta[0] * x ** 2 + theta[1] * x + theta[2]
+    plt.plot(x, y, 'r-', linewidth=2, label=u'Actual')
+    plt.plot(x, y_hat, 'g-', linewidth=2, label=u'Predict')
+    plt.legend(loc='upper left')
+    plt.grid()
+    plt.show()
 
-    # # 8.2 使用scipy计算函数极值
-    # a = opt.fmin(f, 1)
-    # b = opt.fmin_cg(f, 1)
-    # c = opt.fmin_bfgs(f, 1)
-    # print a, 1/a, math.e
-    # print b
-    # print c
+    # 线性回归例2
+    x = np.linspace(0, 5, 100)
+    A = 5
+    w = 1.5
+    y = A * np.sin(w * x) + np.random.rand(len(x)) - 0.5
+    t = leastsq(residual2, [3, 1], args=(x, y))
+    theta = t[0]
+    print('真实值：', A, w)
+    print('预测值：', theta)
+    y_hat = theta[0] * np.sin(theta[1] * x)
+    plt.plot(x, y, 'r-', linewidth=2, label='Actual')
+    plt.plot(x, y_hat, 'g-', linewidth=2, label='Predict')
+    plt.legend(loc='lower left')
+    plt.grid()
+    plt.show()
 
+    # 8.2 使用scipy计算函数极值
+    import math
+
+    a = opt.fmin(f, 1)
+    b = opt.fmin_cg(f, 1)
+    c = opt.fmin_bfgs(f, 1)
+    print(a, 1 / a, math.e)
+    print(b)
+    print(c)
+    '''
     # marker	description
     # ”.”	point
     # ”,”	pixel
--- 1.Python基础+数学库/(4) stat.py
+++ 1.Python基础+数学库/(4) stat.py
@@ -6,6 +6,7 @@ import math
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from scipy import stats
 
