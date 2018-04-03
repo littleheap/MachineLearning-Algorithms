9dae52122363d7c71788b8c1f259045747d2ae0a
LittleHeap <418761605@qq.com>
2018-04-03 22:50:02 +0800

    update

--- 10.LDA主题模型/10.1 LDA_intro.py
+++ 10.LDA主题模型/10.1 LDA_intro.py
@@ -1,61 +1,105 @@
-# !/usr/bin/python
 # -*- coding:utf-8 -*-
 
 from gensim import corpora, models, similarities
 from pprint import pprint
 
+# 配置输出结果
 # import logging
 # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
 if __name__ == '__main__':
-    f = open('22.LDA_test.txt')
+
+    # 9个英文文档数据，每一行即一个文档
+    f = open('LDA_test.txt')
+    # 创造停止词
     stop_list = set('for a of the and to in'.split())
+
+    # 读取每一行文本数据，去掉两边空格，不考虑停止词，输出原文本分词
     # texts = [line.strip().split() for line in f]
-    # print(texts)
+    # pprint(texts)
+
+    # 读取每一行文本数据，去掉两边空格，转小写，分开，考虑停止词，输出文本分词（除去停止词）
     texts = [[word for word in line.strip().lower().split() if word not in stop_list] for line in f]
     print('Text = ')
     pprint(texts)
 
+    # 生成词典
     dictionary = corpora.Dictionary(texts)
+    # 获取词典中词的个数
     V = len(dictionary)
+    # 实际分词数据转换成词典向量语料库模型
     corpus = [dictionary.doc2bow(text) for text in texts]
+    # 建立TF-IDF预料模型，此模型对原始词典向量语料加权处理了
     corpus_tfidf = models.TfidfModel(corpus)[corpus]
 
+    # 输出原始词典向量语料库，非稀疏矩阵
+    print('Initial Vector Data:')
+    for c in corpus:
+        print(c)
+
+    # 输出TF-IDF
     print('TF-IDF:')
     for c in corpus_tfidf:
         print(c)
 
-    print('\nLSI Model:')
+    # LSI模型（即LSA隐语义分析模型）
+    print('\n---------------LSI Model---------------\n')
+
+    # 设置语料库为TF-IDF，主题数为2，词典为dictionary
     lsi = models.LsiModel(corpus_tfidf, num_topics=2, id2word=dictionary)
+
+    # 遍历获取所有文档语料的主题
     topic_result = [a for a in lsi[corpus_tfidf]]
+
+    print('LSI Topics Result 文档的主题分布:')
     pprint(topic_result)
-    print('LSI Topics:')
+
+    print('LSI Topics Content 主题下的词分布（取前5个词相关度）:')
     pprint(lsi.print_topics(num_topics=2, num_words=5))
-    similarity = similarities.MatrixSimilarity(lsi[corpus_tfidf])  # similarities.Similarity()
-    print('Similarity:')
+
+    # 根据主题计算文档间的相似度
+    similarity = similarities.MatrixSimilarity(lsi[corpus_tfidf])
+    print('LSI Similarity:')
     pprint(list(similarity))
 
-    print('\nLDA Model:')
+    # LDA模型
+    print('\n---------------LDA Model---------------:')
+
+    # 指定主题个数
     num_topics = 2
     lda = models.LdaModel(corpus_tfidf, num_topics=num_topics, id2word=dictionary,
-                          alpha='auto', eta='auto', minimum_probability=0.001)
+                          alpha='auto', eta='auto', minimum_probability=0.001)  # 主题小于0.001则忽略
+
+    # 模型得到后，文档放进去，返回文档对应的主题
     doc_topic = [doc_t for doc_t in lda[corpus_tfidf]]
-    print('Document-Topic:\n')
+    print('LDA Topics Result 文档的主题分布:')
     pprint(doc_topic)
-    for doc_topic in lda.get_document_topics(corpus_tfidf):
-        print(doc_topic)
+
+    # for doc_topic in lda.get_document_topics(corpus_tfidf):
+    #     print(doc_topic)
+
+    # 显示主题内部的词分布，即相关度
     for topic_id in range(num_topics):
-        print('Topic', topic_id)
+        print('LDA Topics Content 主题下的词分布:', topic_id)
         # pprint(lda.get_topic_terms(topicid=topic_id))
         pprint(lda.show_topic(topic_id))
+
+    # 根据主题计算文档间的相似度
     similarity = similarities.MatrixSimilarity(lda[corpus_tfidf])
-    print('Similarity:')
+    print('LDA Similarity:')
     pprint(list(similarity))
 
+    # HDA模型
+    print('\n---------------HDA Model---------------:')
+
     hda = models.HdpModel(corpus_tfidf, id2word=dictionary)
+
+    # 获取HDA分析的每个文本的主题分布
     topic_result = [a for a in hda[corpus_tfidf]]
-    print('\n\nUSE WITH CARE--\nHDA Model:')
+
+    print('HDA Topics Result 文档的主题分布:')
     pprint(topic_result)
-    print('HDA Topics:')
+
+    print('HDA Topics Content 主题下的词分布（取前5个词相关度）:')
     print(hda.print_topics(num_topics=2, num_words=5))
--- 10.LDA主题模型/10.2 netease_news.py
+++ 10.LDA主题模型/10.2 netease_news.py
@@ -1,4 +1,3 @@
-# !/usr/bin/python
 # -*- coding:utf-8 -*-
 
 import numpy as np
@@ -11,8 +10,9 @@ import time
 # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
+# 停止词加载函数
 def load_stopword():
-    f_stop = open('22.stopword.txt')
+    f_stop = open('stopword.txt')
     sw = [line.strip() for line in f_stop]
     f_stop.close()
     return sw
@@ -20,63 +20,98 @@ def load_stopword():
 
 if __name__ == '__main__':
     print('初始化停止词列表 --')
+
+    # 记录开始时间
     t_start = time.time()
+
+    # 获取停止词
     stop_words = load_stopword()
 
     print('开始读入语料数据 -- ')
-    f = open('news.dat')  # 22.LDA_test.txt
+    f = open('news.dat', encoding='utf-8')  # LDA_test.txt
+
+    # 使用停止词分割
     texts = [[word for word in line.strip().lower().split() if word not in stop_words] for line in f]
     # texts = [line.strip().split() for line in f]
     print('读入语料数据完成，用时%.3f秒' % (time.time() - t_start))
+
+    # 关闭文件流
     f.close()
+
+    # 获取文本数量
     M = len(texts)
     print('文本数目：%d个' % M)
     # pprint(texts)
 
     print('正在建立词典 --')
     dictionary = corpora.Dictionary(texts)
+
+    # 获取词典长度
     V = len(dictionary)
+    print('词典中词的个数：', V)
+
+    # 计算文本向量
     print('正在计算文本向量 --')
     corpus = [dictionary.doc2bow(text) for text in texts]
+
+    # 计算TF-IDF
     print('正在计算文档TF-IDF --')
     t_start = time.time()
-    corpus_tfidf = models.TfidfModel(corpus)[corpus]
+    corpus_tfidf = models.TfidfModel(corpus)[corpus]  # 喂养数据
     print('建立文档TF-IDF完成，用时%.3f秒' % (time.time() - t_start))
+
     print('LDA模型拟合推断 --')
-    num_topics = 30
+    # 设置主题数目
+    num_topics = 10
+
     t_start = time.time()
+
+    # 构建LDA模型
     lda = models.LdaModel(corpus_tfidf, num_topics=num_topics, id2word=dictionary,
                           alpha=0.01, eta=0.01, minimum_probability=0.001,
                           update_every=1, chunksize=100, passes=1)
     print('LDA模型完成，训练时间为\t%.3f秒' % (time.time() - t_start))
-    # # 所有文档的主题
+
+    # 所有文档的主题分布
     # doc_topic = [a for a in lda[corpus_tfidf]]
-    # print 'Document-Topic:\n'
+    # print('Document-Topic:\n')
     # pprint(doc_topic)
 
     # 随机打印某10个文档的主题
     num_show_topic = 10  # 每个文档显示前几个主题
     print('10个文档的主题分布：')
-    doc_topics = lda.get_document_topics(corpus_tfidf)  # 所有文档的主题分布
+    # 所有文档的主题分布
+    doc_topics = lda.get_document_topics(corpus_tfidf)
+    # 0~(M-1)的数组
     idx = np.arange(M)
+    # 乱序
     np.random.shuffle(idx)
+    # 取前十个不重复数字
     idx = idx[:10]
     for i in idx:
+        # i号文档的主题分布
         topic = np.array(doc_topics[i])
+        # 只获取当前主题分布的分布概率数据，即第二列，第一列为0~9序号忽略
         topic_distribute = np.array(topic[:, 1])
-        # print topic_distribute
+        # print(topic_distribute)
+        # 主题分布排序
         topic_idx = topic_distribute.argsort()[:-num_show_topic - 1:-1]
         print(('第%d个文档的前%d个主题：' % (i, num_show_topic)), topic_idx)
         print(topic_distribute[topic_idx])
+
     num_show_term = 7  # 每个主题显示几个词
     print('每个主题的词分布：')
     for topic_id in range(num_topics):
         print('主题#%d：\t' % topic_id)
+        # LDA第id号主题对应的词分布
         term_distribute_all = lda.get_topic_terms(topicid=topic_id)
+        # 取前7个词
         term_distribute = term_distribute_all[:num_show_term]
+        # 转换array形式
         term_distribute = np.array(term_distribute)
         term_id = term_distribute[:, 0].astype(np.int)
         print('词：\t', )
         for t in term_id:
+            # 从词典中取出对应的词显示
             print(dictionary.id2token[t], )
         # print('\n概率：\t', term_distribute[:, 1])
